23b083c4e6913536ee6013047f3bf8d1ba64480a
MBrian Website <muhammad.brian.27@gmail.com>
2023-02-20 06:07:51 +0700

    SET UP FACTORY AND SEEDER

---
+++ database/seeders/CompanyUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CompanyUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 30; $i++) {
+            DB::table('company_user')->insert([
+                'user_id' => rand(1, 10),
+                'company_id' => rand(1, 10),
+            ]);
+        }
+    }
+}
